e9641883052243aaf9e1a662c5e7e7db8dc38081
KshipraShetty <kshiprashetty@gmail.com>
2018-01-23 12:57:49 +0530

    Delete program9.js

--- program9.js
+++
@@ -1,7 +0,0 @@
-module.exports = (val, transform = x => x) => {
-  if (typeof (transform) !== 'number') {
-    return `${val}${'!'.repeat(val.length)}`;
-  }
-  
-    return `${val}${'!'.repeat(transform)}`;
-};
