b61cb4fd337602c7668f35077b95247dcbdfaaea
kshiprashetty <kshiprashetty@gmail.com>
2018-01-23 12:41:24 +0530

    new test cases

--- bowling.test.js
+++ bowling.test.js
@@ -8,7 +8,25 @@ test('Check for base case with no strikes and spares', () => {
   expect(a([3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6])).toBe(90);
 });
 
+test('Check for case with spare in between', () => {
+  expect(a([3, 6, 4, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6])).toBe(94);
+});
 
+test('Check for case with spare at the beginning', () => {
+  expect(a([3, 6, 6, 4, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6])).toBe(94);
+});
+
+test('Check for case with strike at the beginning', () => {
+  expect(a([10, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6])).toBe(100);
+});
+
+test('Check for case with strike in between', () => {
+  expect(a([3, 6, 3, 6, 10, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6])).toBe(100);
+});
+
+test('Check for not a spare but looks like spare', () => {
+  expect(a([3, 6, 1, 8, 2, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6])).toBe(89);
+});
 /*
 descibe('check for empty input', () =>
   it('Result should be 0', () =>
